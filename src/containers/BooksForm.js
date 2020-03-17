2fa55a03bf4eda682cff5f97598ba9c20b6c6a01
Eva Veskova Jackson <51458322+evaveskova@users.noreply.github.com>
2020-03-17 21:32:54 +0100

    error fix

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -6,10 +6,10 @@ const BooksForm = () => {
   /* eslint-disable jsx-a11y/label-has-associated-control */
   return (
     <form action="">
-      <label htmlFor="title">Title:</label>
+      <label htmlFor="title" id="title-form-label">Title:</label>
       <input type="text" id="title" />
 
-      <label htmlFor="category">Category:</label>
+      <label htmlFor="category" id="category-form-label">Category:</label>
       <select id="category">
         { categories.map(category => <option value={category} key={`${category}_option`}>{category}</option>)}
       </select>
